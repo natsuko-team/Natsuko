3db168de67136e3c05f2a07cb4bcd75e885b06a2
craftxbox <craftxbox@craftxbox.com>
2019-06-27 23:21:23 -0230

    pongmods yee

--- src/ninja/natsuko/bot/commands/PingModsCommand.java
+++ src/ninja/natsuko/bot/commands/PingModsCommand.java
@@ -1,7 +1,16 @@
 package ninja.natsuko.bot.commands;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.presence.Status;
+import discord4j.core.object.util.Snowflake;
 import ninja.natsuko.bot.Main;
+import ninja.natsuko.bot.util.Utilities;
 
 public class PingModsCommand extends Command {
 
@@ -11,10 +20,15 @@ public class PingModsCommand extends Command {
 
 	@Override
 	public void execute(String[] args, MessageCreateEvent e) {
-		Long modRole = Main.db.getCollection("guilds").find(org.bson.Document.parse("")).first().getLong("modRole");
+		Map<String,Object> opts = Main.db.getCollection("guilds").find(Utilities.guildToFindDoc(e.getGuild().block())).first().get("options", new HashMap<>());
+		Long modRole = (Long) opts.getOrDefault("modrole",null);
 		if(modRole == null) {
-			
+			Utilities.reply(e.getMessage(), "The Moderators role hasnt been set! Set it with `n;config set modrole <mod role id or mention>`!");
+			return;
 		}
+		List<Member> mods = e.getGuild().block().getMembers().filter(a->a.getRoleIds().contains(Snowflake.of(modRole))).filter(a->a.getPresence().block().getStatus().equals(Status.ONLINE)).collect(Collectors.toList()).block();
+		Member mod = mods.get(0);
+		Utilities.reply(e.getMessage(), "Mod Autoping: "+mod.getMention()+"\n**"+String.join(" ", args)+"**\nFrom: **"+e.getMember().get().getUsername()+"#"+e.getMember().get().getDiscriminator()+"** ("+e.getMember().get().getId().asString()+")");
 	}
 
 }
