329b857762c1c931241d91eae8d3465bec4132fd
craftxbox <craftxbox@craftxbox.com>
2019-06-27 22:04:36 -0230

    magic returns

--- src/ninja/natsuko/bot/commands/KickCommand.java
+++ src/ninja/natsuko/bot/commands/KickCommand.java
@@ -25,15 +25,19 @@ public class KickCommand extends Command {
 				Member target = ArgumentParser.toMemberByID(actualArgs.get(0).replaceAll("[<!@>]", ""), e.getGuild().block());
 				if(target.isHigher(e.getMember().get()).block()) {
 					Utilities.reply(e.getMessage(), "That user is above you!");
+					return;
 				}
 				if(target.isHigher(e.getGuild().block().getMemberById(e.getClient().getSelfId().get()).block()).block()) {
 					Utilities.reply(e.getMessage(), "That user is above the bot!");
+					return;
 				}
 				target.kick("["+e.getMember().get().getUsername()+"#"+e.getMember().get().getDiscriminator()+" ("+e.getMember().get().getId().asString()+") ] "+String.join(" ", args).substring(args[0].length()+1)).subscribe();
 				//TODO properly modlog it @lewistehminerz
 				if(!actualArgs.get(1).matches("-s|--silent")) {
 					Utilities.reply(e.getMessage(), e.getMember().get().getMention() + " Kicked "+target.getUsername());
+					return;
 				}
+				return;
 			}
 			List<Member> partialresult = ArgumentParser.toMemberByPartial(actualArgs.get(0), e.getGuild().block());	
 			if(partialresult.size() > 1) {
@@ -44,18 +48,23 @@ public class KickCommand extends Command {
 			Member target = partialresult.get(0);
 			if(target.isHigher(e.getMember().get()).block()) {
 				Utilities.reply(e.getMessage(), "That user is above you!");
+				return;
 			}
 			if(target.isHigher(e.getGuild().block().getMemberById(e.getClient().getSelfId().get()).block()).block()) {
 				Utilities.reply(e.getMessage(), "That user is above the bot!");
+				return;
 			}
-			if(!e.getGuild().block().getMemberById(e.getClient().getSelfId().get()).block().getBasePermissions().block().contains(Permission.KICK_MEMBERS)) {
-				Utilities.reply(e.getMessage(), "I don't have permissions to kick!");
+			if(!(e.getGuild().block().getMemberById(e.getClient().getSelfId().get()).block().getBasePermissions().block().contains(Permission.KICK_MEMBERS))) {
+				Utilities.reply(e.getMessage(), "I don't have permissions to kick!");	
+				return;
 			}
 			target.kick("["+e.getMember().get().getUsername()+"#"+e.getMember().get().getDiscriminator()+" ("+e.getMember().get().getId().asString()+") ] "+String.join(" ", args).substring(args[0].length()+1)).subscribe();
 			//TODO properly modlog it @lewistehminerz
 			if(!actualArgs.get(1).matches("-s|--silent")) {
 				Utilities.reply(e.getMessage(), e.getMember().get().getMention() + " Kicked "+target.getUsername());
+				return;
 			}
+			return;
 		}
 		Utilities.reply(e.getMessage(), "You dont have permissions to kick!");
 		return;
