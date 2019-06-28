620c8c6320290f9b6796f35125ba3bb3925d9778
craftxbox <craftxbox@craftxbox.com>
2019-06-27 21:57:12 -0230

    properl  regex

--- src/ninja/natsuko/bot/util/ArgumentParser.java
+++ src/ninja/natsuko/bot/util/ArgumentParser.java
@@ -17,7 +17,7 @@ public class ArgumentParser {
 
 	public static List<String> toArgs(String string){
 		List<String> temp = new ArrayList<>();
-		Matcher baseMatcher = Pattern.compile("").matcher(string);
+		Matcher baseMatcher = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(string);
 		while (baseMatcher.find()) {
 		    temp.add(baseMatcher.group(1));
 		}
