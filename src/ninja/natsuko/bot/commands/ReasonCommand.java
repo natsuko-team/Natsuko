ec5a3780a3d60454df23c12b0369f7a17c670289
craftxbox <craftxbox@craftxbox.com>
2019-06-28 22:44:18 -0230

    reason updates modlog

--- src/ninja/natsuko/bot/commands/ReasonCommand.java
+++ src/ninja/natsuko/bot/commands/ReasonCommand.java
@@ -1,13 +1,22 @@
 package ninja.natsuko.bot.commands;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.bson.Document;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Channel;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.util.Snowflake;
 import ninja.natsuko.bot.Main;
+import ninja.natsuko.bot.moderation.Case;
+import ninja.natsuko.bot.moderation.Case.CaseType;
 import ninja.natsuko.bot.util.ArgumentParser;
 import ninja.natsuko.bot.util.Utilities;
 
@@ -30,6 +39,12 @@ public class ReasonCommand extends Command {
 		Document guildoc = Main.db.getCollection("guilds").find(Utilities.guildToFindDoc(e.getGuild().block())).first();
 		List<Document> modlog = guildoc.get("modlog", new ArrayList<>());
 		Document theCase = modlog.stream().filter(d->d.getInteger("id").equals(ArgumentParser.toInt(args[0]))).collect(Collectors.toList()).get(0);
+		Map<String,Object> opts = Main.db.getCollection("guilds").find(Utilities.guildToFindDoc(e.getGuild().block())).first().get("options", new HashMap<>());
+		TextChannel chan = (TextChannel) e.getClient().getChannelById(Snowflake.of((long) opts.get("modlog"))).block();
+		Message mesg = chan.getMessageById(Snowflake.of(theCase.getLong("msgid"))).block();
+		mesg.edit(m->{
+			m.setContent(new Case(Main.client.getUserById(Snowflake.of(theCase.get("targetUser").toString())).block(),Main.client.getUserById(Snowflake.of(theCase.get("moderatorUser").toString())).block(),Instant.ofEpochMilli(theCase.getLong("date")),CaseType.valueOf(theCase.get("type").toString()),theCase.get("expiryDate")==null?null:Instant.ofEpochMilli(theCase.getLong("expiryDate")),theCase.get("reason").toString(),theCase.getInteger("id")).toModLog());
+		}).subscribe();
 		int i = modlog.indexOf(theCase);
 		theCase.put("reason",String.join(" ", args).substring(args[0].length()+1));
 		modlog.set(i, theCase);
