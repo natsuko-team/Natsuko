2fa704bb84555d78a22655e030ecc282e2c92d13
craftxbox <craftxbox@craftxbox.com>
2019-06-29 02:33:24 -0230

    fuckit just epoch

--- src/ninja/natsuko/bot/commands/ServerCommand.java
+++ src/ninja/natsuko/bot/commands/ServerCommand.java
@@ -1,8 +1,5 @@
 package ninja.natsuko.bot.commands;
 
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.User;
 import ninja.natsuko.bot.util.Utilities;
@@ -18,12 +15,11 @@ public class ServerCommand extends Command {
 		Utilities.reply(e.getMessage(), spec -> {
 			spec.setEmbed(embed -> {
 				User owner = e.getGuild().block().getOwner().block();
-				DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
 				embed.setAuthor("Natsuko", "https://natsuko.ninja", "https://natsuko.ninja");
 				embed.addField("Owner", owner.getUsername()+"#"+owner.getDiscriminator(), true);
 				embed.addField("Name", e.getGuild().block().getName().toString(), true);
 				embed.addField("Members", e.getGuild().block().getMembers().count().block().toString(), true);
-				embed.addField("Date Added", formatter.format(e.getGuild().block().getJoinTime().get()), true);
+				embed.addField("Date Added", Long.toString( e.getGuild().block().getJoinTime().get().toEpochMilli()), true);
 				embed.setColor(Utilities.embedColor);
 			});
 		});
