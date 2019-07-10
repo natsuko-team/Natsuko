a6b47be5eab12c80e0e4352ee49a537a2a6bf618
craftxbox <craftxbox@craftxbox.com>
2019-07-10 17:28:15 -0230

    begin on scriptengine mute and strike

--- src/ninja/natsuko/bot/scriptengine/SafeMember.java
+++ src/ninja/natsuko/bot/scriptengine/SafeMember.java
@@ -1,9 +1,18 @@
 package ninja.natsuko.bot.scriptengine;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bson.Document;
 
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.util.Snowflake;
+import ninja.natsuko.bot.Main;
+import ninja.natsuko.bot.moderation.ModLogger;
+import ninja.natsuko.bot.moderation.Case.CaseType;
+import ninja.natsuko.bot.util.Utilities;
 
 public class SafeMember {
 
@@ -19,6 +28,54 @@ public class SafeMember {
 	
 	public void kick(String reason) {
 		this.member.kick(reason).block();
+	}
+	
+	public void strike(String reason, int amount) {
+		Document guildoc = Main.db.getCollection("guilds").find(Utilities.guildToFindDoc(this.member.getGuild().block())).first();
+		List<Document> strikes = guildoc.get("strikes", new ArrayList<>());
+		List<Document> temp = strikes.stream().filter(a->a.getLong("id") == this.member.getId().asLong()).collect(Collectors.toList());
+		Document userStrikes;
+		if(temp.size() < 1) {
+			userStrikes = Document.parse("{\"id\":"+this.member.getId().asLong()+",\"strikes\":"+amount+"}");
+			strikes.add(userStrikes);
+		} else {
+			userStrikes = temp.get(0);
+			int i = strikes.indexOf(userStrikes);
+			userStrikes.put("strikes", userStrikes.getInteger("strikes")+amount);
+			strikes.set(i, userStrikes);
+		}
+		guildoc.put("strikes", strikes);
+		Main.db.getCollection("guilds").replaceOne(Utilities.guildToFindDoc(this.member.getGuild().block()), guildoc);
+		Utilities.processStrike(this.member,userStrikes.getInteger("strikes"));
+		ModLogger.logCase(this.member.getGuild().block(), ModLogger.newCase(this.member, Main.client.getSelf().block(), reason, null, CaseType.STRIKE, amount, this.member.getGuild().block()));
+	}
+	
+	public void unstrike(String reason, int amount) {
+		Document guildoc = Main.db.getCollection("guilds").find(Utilities.guildToFindDoc(this.member.getGuild().block())).first();
+		List<Document> strikes = guildoc.get("strikes", new ArrayList<>());
+		List<Document> temp = strikes.stream().filter(a->a.getLong("id") == this.member.getId().asLong()).collect(Collectors.toList());
+		Document userStrikes;
+		if(temp.size() < 1) {
+			userStrikes = Document.parse("{\"id\":"+this.member.getId().asLong()+",\"strikes\":"+amount+"}");
+			strikes.add(userStrikes);
+		} else {
+			userStrikes = temp.get(0);
+			int i = strikes.indexOf(userStrikes);
+			userStrikes.put("strikes", userStrikes.getInteger("strikes")-amount);
+			strikes.set(i, userStrikes);
+		}
+		guildoc.put("strikes", strikes);
+		Main.db.getCollection("guilds").replaceOne(Utilities.guildToFindDoc(this.member.getGuild().block()), guildoc);
+		Utilities.processStrike(this.member,userStrikes.getInteger("strikes"));
+		ModLogger.logCase(this.member.getGuild().block(), ModLogger.newCase(this.member, Main.client.getSelf().block(), reason, null, CaseType.UNSTRIKE, amount, this.member.getGuild().block()));
+	}
+	
+	public void mute(String reason,int deleteMessages) {
+		
+	}
+	
+	public void unmute(String reason) {
+		
 	}
 	
 	public String getName() {
