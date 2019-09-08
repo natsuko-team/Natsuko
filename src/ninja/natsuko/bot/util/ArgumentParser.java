654b824a738bae5d9bd1318407024d4b3a1271bb
craftxbox <craftxbox@craftxbox.com>
2019-09-08 00:02:50 -0230

    attept 3

--- src/ninja/natsuko/bot/util/ArgumentParser.java
+++ src/ninja/natsuko/bot/util/ArgumentParser.java
@@ -17,7 +17,7 @@ public class ArgumentParser {
 
 	public static List<String> toArgs(String string){
 		List<String> temp = new ArrayList<>();
-		Matcher baseMatcher = Pattern.compile("(?<=^|(?<=[^\\\\])(?:\\\\\\\\)*)([\"']|`(?:``))(?:|.*?(?<=[^\\\\])(?:\\\\\\\\)*)\\1|\\S+").matcher(string);
+		Matcher baseMatcher = Pattern.compile("(?<=^|(?<=[^\\\\])(?:\\\\\\\\){0,1000})([\"']|`(?:``))(?:|.*?(?<=[^\\\\])(?:\\\\\\\\)*)\\1|\\S+").matcher(string);
 		while (baseMatcher.find()) {
 			String match = baseMatcher.group(1);
 			if(match.matches("^\".+\"$")) match = match.substring(1, match.length()-1);
